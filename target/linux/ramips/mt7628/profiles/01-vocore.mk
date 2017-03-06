f56e2bf8921d7b7e8b490423f06198bd5e11a59e
Noble Pepper <gitmail@noblepepper.com>
2017-03-05 20:26:26 -0600

    Rework image setup, backport spi patch from lede, add images for lite, spidev, flashwrite

--- target/linux/ramips/mt7628/profiles/01-vocore.mk
+++ target/linux/ramips/mt7628/profiles/01-vocore.mk
@@ -5,14 +5,153 @@
 # See /LICENSE for more information.
 #
 
-define Profile/VOCORE2	
+define Profile/VOCORE2-BETA
+	NAME:=VoCore2-beta
+	PACKAGES:=\
+		kmod-usb-core kmod-usb2 kmod-usb-ohci \
+		kmod-ledtrig-usbdev \
+		gdbserver curl strace coreutils coreutils-stty \
+	  	avahi-nodbus-daemon mountd mjpg-streamer \
+		uhttpd rpcd rpcd-mod-iwinfo git git-http samba36-server \
+		luci luci-theme-openwrt luci-app-mjpg-streamer luci-app-samba luci-lib-json \
+		rpcd-mod-rpcsys cgi-io spi-tools uboot-envtools \
+		kmod-fs-vfat kmod-fs-exfat kmod-i2c-core kmod-i2c-ralink \
+		kmod-nls-base kmod-nls-cp437 kmod-nls-iso8859-1 \
+		kmod-nls-iso8859-15 kmod-nls-iso8859-2 kmod-nls-utf8 \
+		kmod-usb-storage \
+		kmod-video-core kmod-video-uvc \
+		mtk-sdk-wifi tcpdump-mini
+
+endef
+
+
+define Profile/VOCORE2-BETA/Description
+	Package set for Vocore2 beta single board model with 64M ram
+endef
+$(eval $(call Profile,VOCORE2-BETA))
+
+define Profile/VOCORE2-LITE
+	NAME:=VoCore2-lite
+	PACKAGES:=\
+		kmod-usb-core kmod-usb2 kmod-usb-ohci \
+		kmod-ledtrig-usbdev \
+		gdbserver curl strace coreutils coreutils-stty \
+	  	avahi-nodbus-daemon mountd mjpg-streamer \
+		uhttpd rpcd rpcd-mod-iwinfo git git-http samba36-server \
+		luci luci-theme-openwrt luci-app-mjpg-streamer luci-app-samba luci-lib-json \
+		rpcd-mod-rpcsys cgi-io spi-tools uboot-envtools \
+		kmod-fs-vfat kmod-fs-exfat kmod-i2c-core kmod-i2c-ralink \
+		kmod-nls-base kmod-nls-cp437 kmod-nls-iso8859-1 \
+		kmod-nls-iso8859-15 kmod-nls-iso8859-2 kmod-nls-utf8 \
+		kmod-usb-storage \
+		kmod-video-core kmod-video-uvc \
+		mtk-sdk-wifi tcpdump-mini
+
+endef
+
+
+define Profile/VOCORE2-LITE/Description
+	Package set for Vocore2 lite single board model with 64M ram
+	and 8M Flash
+endef
+
+$(eval $(call Profile,VOCORE2-LITE))
+
+define Profile/VOCORE2-128M
 	NAME:=VoCore2
 	PACKAGES:=\
 		kmod-usb-core kmod-usb2 kmod-usb-ohci \
-		kmod-ledtrig-usbdev
+		kmod-ledtrig-usbdev \
+		gdbserver curl strace coreutils coreutils-stty \
+	  	avahi-nodbus-daemon mountd mjpg-streamer \
+		uhttpd rpcd rpcd-mod-iwinfo git git-http samba36-server \
+		luci luci-theme-openwrt luci-app-mjpg-streamer luci-app-samba luci-lib-json \
+		rpcd-mod-rpcsys cgi-io spi-tools uboot-envtools \
+		kmod-fs-vfat kmod-fs-exfat kmod-i2c-core kmod-i2c-ralink \
+		kmod-nls-base kmod-nls-cp437 kmod-nls-iso8859-1 \
+		kmod-nls-iso8859-15 kmod-nls-iso8859-2 kmod-nls-utf8 \
+		kmod-usb-storage \
+		kmod-video-core kmod-video-uvc \
+		mtk-sdk-wifi tcpdump-mini
+
 endef
 
-define Profile/VOCORE2/Description
-	Package set for Vocore2
+define Profile/VOCORE2-128M/Description
+	Package set for Vocore2 release single board model or AirPlay 
+	model with 128MB ram
 endef
-$(eval $(call Profile,VOCORE2))
+$(eval $(call Profile,VOCORE2-128M))
+
+define Profile/VOCORE2-128M-SD	
+	NAME:=VoCore2-Ulitimate
+	PACKAGES:=\
+		kmod-usb-core kmod-usb2 kmod-usb-ohci \
+		kmod-ledtrig-usbdev \
+	  	gdbserver curl strace coreutils coreutils-stty \
+		avahi-nodbus-daemon mountd mjpg-streamer \
+		uhttpd rpcd rpcd-mod-iwinfo git git-http samba36-server \
+		luci luci-theme-openwrt luci-app-mjpg-streamer luci-app-samba luci-lib-json \
+		rpcd-mod-rpcsys cgi-io spi-tools uboot-envtools \
+		kmod-fs-vfat kmod-fs-exfat kmod-i2c-core kmod-i2c-ralink \
+		kmod-nls-base kmod-nls-cp437 kmod-nls-iso8859-1 \
+		kmod-nls-iso8859-15 kmod-nls-iso8859-2 kmod-nls-utf8 \
+		kmod-sdhci-mt7620 kmod-usb-storage \
+		kmod-video-core kmod-video-uvc \
+		mtk-sdk-wifi tcpdump-mini
+
+endef
+
+define Profile/VOCORE2-128M-SD/Description
+	Package set for Vocore2 release ultimate model with 128M ram and SD slot
+endef
+$(eval $(call Profile,VOCORE2-128M-SD))
+
+define Profile/VOCORE2-128M-SPIDEV
+	NAME:=VoCore2-128M-Spidev
+	PACKAGES:=\
+		kmod-usb-core kmod-usb2 kmod-usb-ohci \
+		kmod-ledtrig-usbdev \
+		gdbserver curl strace coreutils coreutils-stty \
+	  	avahi-nodbus-daemon mountd mjpg-streamer \
+		uhttpd rpcd rpcd-mod-iwinfo git git-http samba36-server \
+		luci luci-theme-openwrt luci-app-mjpg-streamer luci-app-samba luci-lib-json \
+		rpcd-mod-rpcsys cgi-io spi-tools uboot-envtools \
+		kmod-fs-vfat kmod-fs-exfat kmod-i2c-core kmod-i2c-ralink \
+		kmod-nls-base kmod-nls-cp437 kmod-nls-iso8859-1 \
+		kmod-nls-iso8859-15 kmod-nls-iso8859-2 kmod-nls-utf8 \
+		kmod-usb-storage kmod-spi-dev \
+		kmod-video-core kmod-video-uvc \
+		mtk-sdk-wifi tcpdump-mini
+
+endef
+
+define Profile/VOCORE2-128M-SPIDEV/Description
+	Package set for Vocore2 release model with 128M ram
+	For spidev driver using chip select 1
+endef
+$(eval $(call Profile,VOCORE2-128M-SPIDEV))
+
+define Profile/VOCORE2-128M-FLASHWRITE
+	NAME:=VoCore2-128M-Flashwrite
+	PACKAGES:=\
+		kmod-usb-core kmod-usb2 kmod-usb-ohci \
+		kmod-ledtrig-usbdev \
+		gdbserver curl strace coreutils coreutils-stty \
+	  	avahi-nodbus-daemon mountd mjpg-streamer \
+		uhttpd rpcd rpcd-mod-iwinfo git git-http samba36-server \
+		luci luci-theme-openwrt luci-app-mjpg-streamer luci-app-samba luci-lib-json \
+		rpcd-mod-rpcsys cgi-io spi-tools uboot-envtools \
+		kmod-fs-vfat kmod-fs-exfat kmod-i2c-core kmod-i2c-ralink \
+		kmod-nls-base kmod-nls-cp437 kmod-nls-iso8859-1 \
+		kmod-nls-iso8859-15 kmod-nls-iso8859-2 kmod-nls-utf8 \
+		kmod-usb-storage \
+		kmod-video-core kmod-video-uvc \
+		mtk-sdk-wifi tcpdump-mini
+
+endef
+
+define Profile/VOCORE2-128M-FLASHWRITE/Description
+	Package set for Vocore2 release model with 128M ram
+	With second flash chip connected to chip select 1
+endef
+$(eval $(call Profile,VOCORE2-128M-FLASHWRITE))
